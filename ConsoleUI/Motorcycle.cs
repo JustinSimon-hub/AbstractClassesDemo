50ed8e4a0cefdf7512a0258c52bcb5f323e54cca
Justin Simon <justinsimoncs@gmail.com>
2025-11-21 09:42:04 -0800

    Created two new classes

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public override void DriveAbstract()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleUI/Vehicle.cs
+++ ConsoleUI/Vehicle.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
-    internal class Vehicule
+    public abstract class Vehicle
     {
+        public int Year { get; set; } = 1979;
+        public string Make { get; set; } = "Explorer";
+
+        public string Model { get; set; } = "Ford";
+
+
+        public abstract void DriveAbstract();
+        
+
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine("Generic car driving");
+        }
+
     }
 }
+
+
+
+/*
+             * Create an abstract class called Vehicle
+             * The vehicle class shall have three string properties: Year, Make, and Model
+             * Set the defaults of the properties to something generic in the Vehicle class
+             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
+             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
+             */
